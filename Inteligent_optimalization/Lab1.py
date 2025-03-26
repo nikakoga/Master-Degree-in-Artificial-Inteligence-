ee17bf055236fbc1357fe35757896fec133939d4
Weronika Koga <weronikakoga@gmail.com>
2025-03-26 12:20:49 +0100

    another conception - dynamic picking + results

--- Inteligent_optimalization/Lab1.py
+++ Inteligent_optimalization/Lab1.py
@@ -49,272 +49,83 @@ class CycleOptimizer:
         plt.xlabel('X')
         plt.ylabel('Y')
 
-class GreedyCycleOptimizer(CycleOptimizer):
-    
+
+class BalancedCycleOptimizer(CycleOptimizer):
+    def insert_best(self, cycle, unvisited, strategy):
+        best_value = float('inf')
+        best_node = None
+        best_pos = None
+
+        for node in unvisited:
+            for i in range(len(cycle)):
+                a, b = cycle[i], cycle[(i + 1) % len(cycle)]
+                increase = self.distance_matrix[a, node] + self.distance_matrix[node, b] - self.distance_matrix[a, b]
+                value = strategy(increase, a, node, b)
+                if value < best_value:
+                    best_value = value
+                    best_node = node
+                    best_pos = i + 1
+
+        return best_node, best_pos
+
+    def build_balanced_cycles(self, strategy):
+        n = len(self.distance_matrix)
+        target1 = n // 2 + n % 2
+        target2 = n // 2
+
+        start1 = random.randint(0, n - 1)
+        start2 = max(range(n), key=lambda i: self.distance_matrix[start1][i])
+
+        cycle1 = [start1, start1]
+        cycle2 = [start2, start2]
+
+        unvisited = set(range(n)) - {start1, start2}
+
+        while len(cycle1) - 1 < target1 or len(cycle2) - 1 < target2:
+            if len(cycle1) - 1 < target1:
+                node, pos = self.insert_best(cycle1, unvisited, strategy)
+                cycle1.insert(pos, node)
+                unvisited.remove(node)
+            if len(cycle2) - 1 < target2 and unvisited:
+                node, pos = self.insert_best(cycle2, unvisited, strategy)
+                cycle2.insert(pos, node)
+                unvisited.remove(node)
+
+        return cycle1[:-1], cycle2[:-1]
+
+
+class GreedyCycleOptimizer(BalancedCycleOptimizer):
     def create_two_cycles(self):
-        n = len(self.distance_matrix)
-        half_n = n // 2 + n % 2
-
-        start_node1 = random.randint(0, n - 1)
-        start_node2 = max(range(n), key=lambda node: self.distance_matrix[start_node1, node])
-
-        # Usuń oba startowe wierzchołki z unvisited
-        unvisited = set(range(n))
-        unvisited.discard(start_node1)
-        unvisited.discard(start_node2)
-
-        # Twórz cykle na kopiach zbioru unvisited
-        cycle1 = self.greedy_cycle(start_node1, half_n, unvisited.copy())
-
-        if start_node2 in cycle1:
-            raise RuntimeError("start_node2 was added to cycle1! This should not happen.")
-
-        cycle2 = self.greedy_cycle(start_node2, n - half_n, unvisited.copy())
-
-        return cycle1, cycle2
+        return self.build_balanced_cycles(lambda inc, a, n, b: inc)
 
 
-    def greedy_cycle(self, start_node, cycle_length, unvisited):
-        n = len(self.distance_matrix)
-        cycle = [start_node]
-        unvisited.discard(start_node)  # Użyj discard zamiast remove, żeby uniknąć KeyError
-
-        while len(cycle) < cycle_length:
-            # Wybierz najbliższy wierzchołek do dowolnego w cycle
-            next_node = min(
-                unvisited,
-                key=lambda node: min(self.distance_matrix[node, cycle[i]] for i in range(len(cycle)))
-            )
-
-            # Znajdź najlepszą pozycję do wstawienia
-            best_position = min(
-                range(len(cycle)),
-                key=lambda i: self.distance_matrix[cycle[i], next_node]
-                            + self.distance_matrix[next_node, cycle[(i + 1) % len(cycle)]]
-                            - self.distance_matrix[cycle[i], cycle[(i + 1) % len(cycle)]]
-            )
-
-            cycle.insert(best_position + 1, next_node)
-            unvisited.remove(next_node)
-
-        return cycle
-
-class NearestNeighborCycleOptimizer(CycleOptimizer):
+class NearestNeighborCycleOptimizer(BalancedCycleOptimizer):
     def create_two_cycles(self):
-        n = len(self.distance_matrix)
-        half_n = n // 2 + n % 2
+        def nearest_insertion_cost(inc, a, n, b):
+            return min(self.distance_matrix[n][a], self.distance_matrix[n][b])
+        return self.build_balanced_cycles(nearest_insertion_cost)
 
-        start_node1 = random.randint(0, n - 1)
-        start_node2 = max(range(n), key=lambda node: self.distance_matrix[start_node1, node])
 
-        unvisited = set(range(n))
-        unvisited.remove(start_node1)
-        unvisited.remove(start_node2)
-
-        cycle1 = self.nearest_neighbor_cycle(start_node1, half_n, unvisited)
-        cycle2 = self.nearest_neighbor_cycle(start_node2, n - half_n, unvisited)
-
-        return cycle1, cycle2
-
-    def nearest_neighbor_cycle(self, start_node, cycle_length, unvisited):
-        cycle = [start_node]
-
-        while len(cycle) < cycle_length:
-            last_node = cycle[-1]
-            next_node = min(unvisited, key=lambda node: self.distance_matrix[last_node, node])
-            cycle.append(next_node)
-            unvisited.remove(next_node)
-
-        return cycle
-
-class RegretCycleOptimizer(CycleOptimizer):
+class RegretCycleOptimizer(BalancedCycleOptimizer):
     def create_two_cycles(self):
-        n = len(self.distance_matrix)
-        half_n = n // 2 + n % 2
+        def regret_strategy(inc, a, n, b):
+            return inc
+        return self.build_balanced_cycles(regret_strategy)
 
-        start_node1 = random.randint(0, n - 1)
-        start_node2 = max(range(n), key=lambda node: self.distance_matrix[start_node1, node])
 
-        unvisited = set(range(n))
-        unvisited.remove(start_node1)
-        unvisited.remove(start_node2)
-
-        cycle1 = self.regret_cycle(start_node1, half_n, unvisited)
-        cycle2 = self.regret_cycle(start_node2, n - half_n, unvisited)
-
-        return cycle1, cycle2
-
-    def regret_cycle(self, start_node, cycle_length, unvisited):
-        n = len(self.distance_matrix)
-        cycle = [start_node]
-
-        while len(cycle) < cycle_length:
-            regrets = []
-            for node in unvisited:
-                best_increase = float('inf')
-                second_best_increase = float('inf')
-                for i in range(len(cycle)):
-                    increase = self.distance_matrix[cycle[i], node] + self.distance_matrix[node, cycle[(i + 1) % len(cycle)]] - self.distance_matrix[cycle[i], cycle[(i + 1) % len(cycle)]]
-                    if increase < best_increase:
-                        second_best_increase = best_increase
-                        best_increase = increase
-                    elif increase < second_best_increase:
-                        second_best_increase = increase
-                regret = second_best_increase - best_increase
-                regrets.append((regret, node))
-            regrets.sort(reverse=True)
-            next_node = regrets[0][1]
-            best_position = min(range(len(cycle)), key=lambda i: self.distance_matrix[cycle[i], next_node] + self.distance_matrix[next_node, cycle[(i + 1) % len(cycle)]] - self.distance_matrix[cycle[i], cycle[(i + 1) % len(cycle)]])
-            cycle.insert(best_position + 1, next_node)
-            unvisited.remove(next_node)
-
-        return cycle
-
-class WeightedRegretCycleOptimizer(CycleOptimizer):
+class WeightedRegretCycleOptimizer(BalancedCycleOptimizer):
     def create_two_cycles(self, weight1=1, weight2=-1):
-        n = len(self.distance_matrix)
-        half_n = n // 2 + n % 2
+        def weighted_regret(inc, a, n, b):
+            return weight1 * inc + weight2 * (self.distance_matrix[a][n] + self.distance_matrix[n][b]) / 2
+        return self.build_balanced_cycles(weighted_regret)
 
-        start_node1 = random.randint(0, n - 1)
-        start_node2 = max(range(n), key=lambda node: self.distance_matrix[start_node1, node])
 
-        unvisited = set(range(n))
-        unvisited.remove(start_node1)
-        unvisited.remove(start_node2)
-
-        cycle1 = self.weighted_regret_cycle(start_node1, half_n, unvisited, weight1, weight2)
-        cycle2 = self.weighted_regret_cycle(start_node2, n - half_n, unvisited, weight1, weight2)
-
-        return cycle1, cycle2
-
-    def weighted_regret_cycle(self, start_node, cycle_length, unvisited, weight1, weight2):
-        n = len(self.distance_matrix)
-        cycle = [start_node]
-
-        while len(cycle) < cycle_length:
-            regrets = []
-            for node in unvisited:
-                best_increase = float('inf')
-                second_best_increase = float('inf')
-                for i in range(len(cycle)):
-                    increase = self.distance_matrix[cycle[i], node] + self.distance_matrix[node, cycle[(i + 1) % len(cycle)]] - self.distance_matrix[cycle[i], cycle[(i + 1) % len(cycle)]]
-                    if increase < best_increase:
-                        second_best_increase = best_increase
-                        best_increase = increase
-                    elif increase < second_best_increase:
-                        second_best_increase = increase
-                regret = weight1 * best_increase + weight2 * second_best_increase
-                regrets.append((regret, node))
-            regrets.sort(reverse=True)
-            next_node = regrets[0][1]
-            best_position = min(range(len(cycle)), key=lambda i: self.distance_matrix[cycle[i], next_node] + self.distance_matrix[next_node, cycle[(i + 1) % len(cycle)]] - self.distance_matrix[cycle[i], cycle[(i + 1) % len(cycle)]])
-            cycle.insert(best_position + 1, next_node)
-            unvisited.remove(next_node)
-
-        return cycle
-    
-# Załaduj problemy z plików .tsp
-optimizer_kroa200_greedy = GreedyCycleOptimizer('kroA200')
-optimizer_krob200_greedy = GreedyCycleOptimizer('kroB200')
-
-optimizer_kroa200_nn = NearestNeighborCycleOptimizer('kroA200')
-optimizer_krob200_nn = NearestNeighborCycleOptimizer('kroB200')
-
-optimizer_kroa200_regret = RegretCycleOptimizer('kroA200')
-optimizer_krob200_regret = RegretCycleOptimizer('kroB200')
-
-optimizer_kroa200_weighted_regret = WeightedRegretCycleOptimizer('kroA200')
-optimizer_krob200_weighted_regret = WeightedRegretCycleOptimizer('kroB200')
-
-# Utwórz dwa cykle dla kroA200 za pomocą różnych metod
-cycle1_kroa200_greedy, cycle2_kroa200_greedy = optimizer_kroa200_greedy.create_two_cycles()
-length1_kroa200_greedy = optimizer_kroa200_greedy.calculate_cycle_length(cycle1_kroa200_greedy)
-length2_kroa200_greedy = optimizer_kroa200_greedy.calculate_cycle_length(cycle2_kroa200_greedy)
-
-cycle1_kroa200_nn, cycle2_kroa200_nn = optimizer_kroa200_nn.create_two_cycles()
-length1_kroa200_nn = optimizer_kroa200_nn.calculate_cycle_length(cycle1_kroa200_nn)
-length2_kroa200_nn = optimizer_kroa200_nn.calculate_cycle_length(cycle2_kroa200_nn)
-
-cycle1_kroa200_regret, cycle2_kroa200_regret = optimizer_kroa200_regret.create_two_cycles()
-length1_kroa200_regret = optimizer_kroa200_regret.calculate_cycle_length(cycle1_kroa200_regret)
-length2_kroa200_regret = optimizer_kroa200_regret.calculate_cycle_length(cycle2_kroa200_regret)
-
-cycle1_kroa200_weighted_regret, cycle2_kroa200_weighted_regret = optimizer_kroa200_weighted_regret.create_two_cycles()
-length1_kroa200_weighted_regret = optimizer_kroa200_weighted_regret.calculate_cycle_length(cycle1_kroa200_weighted_regret)
-length2_kroa200_weighted_regret = optimizer_kroa200_weighted_regret.calculate_cycle_length(cycle2_kroa200_weighted_regret)
-
-# Utwórz dwa cykle dla kroB200 za pomocą różnych metod
-cycle1_krob200_greedy, cycle2_krob200_greedy = optimizer_krob200_greedy.create_two_cycles()
-length1_krob200_greedy = optimizer_krob200_greedy.calculate_cycle_length(cycle1_krob200_greedy)
-length2_krob200_greedy = optimizer_krob200_greedy.calculate_cycle_length(cycle2_krob200_greedy)
-
-cycle1_krob200_nn, cycle2_krob200_nn = optimizer_krob200_nn.create_two_cycles()
-length1_krob200_nn = optimizer_krob200_nn.calculate_cycle_length(cycle1_krob200_nn)
-length2_krob200_nn = optimizer_krob200_nn.calculate_cycle_length(cycle2_krob200_nn)
-
-cycle1_krob200_regret, cycle2_krob200_regret = optimizer_krob200_regret.create_two_cycles()
-length1_krob200_regret = optimizer_krob200_regret.calculate_cycle_length(cycle1_krob200_regret)
-length2_krob200_regret = optimizer_krob200_regret.calculate_cycle_length(cycle2_krob200_regret)
-
-cycle1_krob200_weighted_regret, cycle2_krob200_weighted_regret = optimizer_krob200_weighted_regret.create_two_cycles()
-length1_krob200_weighted_regret = optimizer_krob200_weighted_regret.calculate_cycle_length(cycle1_krob200_weighted_regret)
-length2_krob200_weighted_regret = optimizer_krob200_weighted_regret.calculate_cycle_length(cycle2_krob200_weighted_regret)
-
-# Wyświetl wyniki
-print("Cycles for kroA200 using Greedy Cycle:")
-print("Cycle 1:", cycle1_kroa200_greedy, "Length:", length1_kroa200_greedy)
-print("Cycle 2:", cycle2_kroa200_greedy, "Length:", length2_kroa200_greedy)
-
-print("\nCycles for kroB200 using Greedy Cycle:")
-print("Cycle 1:", cycle1_krob200_greedy, "Length:", length1_krob200_greedy)
-print("Cycle 2:", cycle2_krob200_greedy, "Length:", length2_krob200_greedy)
-
-print("\nCycles for kroA200 using Nearest Neighbor:")
-print("Cycle 1:", cycle1_kroa200_nn, "Length:", length1_kroa200_nn)
-print("Cycle 2:", cycle2_kroa200_nn, "Length:", length2_kroa200_nn)
-
-print("\nCycles for kroB200 using Nearest Neighbor:")
-print("Cycle 1:", cycle1_krob200_nn, "Length:", length1_krob200_nn)
-print("Cycle 2:", cycle2_krob200_nn, "Length:", length2_krob200_nn)
-
-print("\nCycles for kroA200 using 2-Regret:")
-print("Cycle 1:", cycle1_kroa200_regret, "Length:", length1_kroa200_regret)
-print("Cycle 2:", cycle2_kroa200_regret, "Length:", length2_kroa200_regret)
-
-print("\nCycles for kroB200 using 2-Regret:")
-print("Cycle 1:", cycle1_krob200_regret, "Length:", length1_krob200_regret)
-print("Cycle 2:", cycle2_krob200_regret, "Length:", length2_krob200_regret)
-
-print("\nCycles for kroA200 using Weighted 2-Regret:")
-print("Cycle 1:", cycle1_kroa200_weighted_regret, "Length:", length1_kroa200_weighted_regret)
-print("Cycle 2:", cycle2_kroa200_weighted_regret, "Length:", length2_kroa200_weighted_regret)
-
-print("\nCycles for kroB200 using Weighted 2-Regret:")
-print("Cycle 1:", cycle1_krob200_weighted_regret, "Length:", length1_krob200_weighted_regret)
-print("Cycle 2:", cycle2_krob200_weighted_regret, "Length:", length2_krob200_weighted_regret)
-
-plt.figure(figsize=(16, 12))
-
-# # kroA200
-optimizer_kroa200_greedy.plot_cycles(cycle1_kroa200_greedy, cycle2_kroa200_greedy, "Greedy Cycle - kroA200", 241)
-optimizer_kroa200_nn.plot_cycles(cycle1_kroa200_nn, cycle2_kroa200_nn, "Nearest Neighbor - kroA200", 242)
-optimizer_kroa200_regret.plot_cycles(cycle1_kroa200_regret, cycle2_kroa200_regret, "2-Regret - kroA200", 243)
-optimizer_kroa200_weighted_regret.plot_cycles(cycle1_kroa200_weighted_regret, cycle2_kroa200_weighted_regret, "Weighted 2-Regret - kroA200", 244)
-
-# kroB200
-optimizer_krob200_greedy.plot_cycles(cycle1_krob200_greedy, cycle2_krob200_greedy, "Greedy Cycle - kroB200", 245)
-optimizer_krob200_nn.plot_cycles(cycle1_krob200_nn, cycle2_krob200_nn, "Nearest Neighbor - kroB200", 246)
-optimizer_krob200_regret.plot_cycles(cycle1_krob200_regret, cycle2_krob200_regret, "2-Regret - kroB200", 247)
-optimizer_krob200_weighted_regret.plot_cycles(cycle1_krob200_weighted_regret, cycle2_krob200_weighted_regret, "Weighted 2-Regret - kroB200", 248)
-
-plt.tight_layout()
-plt.show()
-
+# === EXPERIMENTAL RESULTS TABLE ===
 def run_experiments(optimizer_class, problem_name, label, runs=100):
     total_lengths = []
 
     for i in range(runs):
-        random.seed(i)  # Ustaw ziarno dla powtarzalności
+        random.seed(i)
         optimizer = optimizer_class(problem_name)
         if isinstance(optimizer, WeightedRegretCycleOptimizer):
             cycle1, cycle2 = optimizer.create_two_cycles(weight1=1, weight2=-1)
@@ -328,18 +139,41 @@ def run_experiments(optimizer_class, problem_name, label, runs=100):
     min_val = int(np.min(total_lengths))
     max_val = int(np.max(total_lengths))
 
-    return f"{label}: {avg} ({min_val} – {max_val})"
+    return f"{avg} ({min_val} – {max_val})"
 
-print("\n=== EXPERIMENTS (100 RUNS) ===")
+methods = [
+    (GreedyCycleOptimizer, "Greedy"),
+    (NearestNeighborCycleOptimizer, "Nearest Neighbor"),
+    (RegretCycleOptimizer, "2-Regret"),
+    (WeightedRegretCycleOptimizer, "Weighted 2-Regret")
+]
 
-print("kroA200:")
-print(run_experiments(GreedyCycleOptimizer, 'kroA200', "Greedy"))
-print(run_experiments(NearestNeighborCycleOptimizer, 'kroA200', "Nearest Neighbor"))
-print(run_experiments(RegretCycleOptimizer, 'kroA200', "2-Regret"))
-print(run_experiments(WeightedRegretCycleOptimizer, 'kroA200', "Weighted 2-Regret"))
+# === OPTIONAL: PLOT EXAMPLES ===
+def plot_example_cycles():
+    fig = plt.figure(figsize=(12, 10))
+    for j, instance in enumerate(["kroA200", "kroB200"]):
+        for i, (optimizer_class, label) in enumerate(methods):
+            optimizer = optimizer_class(instance)
+            if isinstance(optimizer, WeightedRegretCycleOptimizer):
+                cycle1, cycle2 = optimizer.create_two_cycles(weight1=1, weight2=-1)
+            else:
+                cycle1, cycle2 = optimizer.create_two_cycles()
+            pos = 4 * j + i + 1  # 4 columns layout: 2x4 (instance x methods)
+            optimizer.plot_cycles(cycle1, cycle2, f"{label} - {instance}", 240 + pos)
+    plt.tight_layout()
+    plt.show()
 
-print("\nkroB200:")
-print(run_experiments(GreedyCycleOptimizer, 'kroB200', "Greedy"))
-print(run_experiments(NearestNeighborCycleOptimizer, 'kroB200', "Nearest Neighbor"))
-print(run_experiments(RegretCycleOptimizer, 'kroB200', "2-Regret"))
-print(run_experiments(WeightedRegretCycleOptimizer, 'kroB200', "Weighted 2-Regret"))
+print("\n=== EXPERIMENTAL RESULTS (100 RUNS) ===")
+
+instances = ["kroA200", "kroB200"]
+
+header = "| Method \t\t\t| kroA200 \t\t\t\t| kroB200 \t\t\t\t|"
+separator = "|-----------------------|-------------------------------|-------------------------------|"
+print(header)
+print(separator)
+
+for optimizer_class, label in methods:
+    results = [run_experiments(optimizer_class, inst, label) for inst in instances]
+    print(f"| {label:<21} | {results[0]:<29} | {results[1]:<29} |")
+
+plot_example_cycles()
